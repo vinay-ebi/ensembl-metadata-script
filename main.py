c53d53d946005fa3dfa3b336b01557bcbe49e729
vinay <vinay@ebi.ac.uk>
2022-11-02 18:31:25 +0000

    script ready to run on hps rrdumps

--- main.py
+++ main.py
@@ -18,14 +18,13 @@ import logging
 import os
 import sys
 from os.path import expanduser
-from metadata import generate_metadata, get_annotations_source, get_annotations_source_async
+from metadata import generate_metadata, get_annotations_source, get_annotations_source_async, move_subdir_paths, set_broken_symlink
 from metadata import MetadataParams
 
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+script_path = os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
+os.chdir(script_path)
 
 logging.basicConfig(
-    # filename='main.log',
-    # filemode='a',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S')
@@ -58,29 +57,62 @@ if __name__ == "__main__":
 
     arguments = parser.parse_args(sys.argv[1:])
     logger.setLevel(logging.INFO)
-    if not os.path.exists(os.path.join(arguments.ftp_path, 'species' )):
-        logger.error(f"No species dir found in provided ftp_path: {arguments.ftp_path}")
+    processed_any_species = False
+    if not os.path.exists(os.path.join(arguments.ftp_path, 'species' )) or not os.path.exists(os.path.join(arguments.ftp_path, 'timestamped/species' )):
+        logger.error(f"No species or timestamped/species dir found in provided ftp_path: {arguments.ftp_path}")
         sys.exit(1)
         
     logger.info("Preparing Directory Path For RR Ftp Dumps With Subdir Annotation Source")
+    logger.info(f"Fetching ensembl metadata with provided params: {arguments}")
     for species_info in generate_metadata(arguments):
-        core_metadata = get_annotations_source(species_info['dbname'], arguments.coredb_url)
-        #if 'species.annotation_source' not in core_metadata.keys() or core_metadata['species.annotation_source'] == '' :
-        if core_metadata.get('species.annotation_source', '')  ==  '':
-            core_metadata['species.annotation_source'] = 'ensembl' 
-                  
-        print("getting core")
-        print(core_metadata)
-        species_name = "_".join(species_info['display_name'].split(' ')[0:2]) #Homo sapiens (Human) - GCA_018503265.1 
-        accession_name = species_info['assembly_accession'] #GCA_009914755.4
-        genebuild_inital = core_metadata.get('genebuild.initial_release_date').replace('-','_') if core_metadata.get('genebuild.initial_release_date', None) else ''
-        genebuild_update = core_metadata.get('genebuild.last_geneset_update').replace('-','_') if core_metadata.get('genebuild.initial_release_date', None) else ''
         
-        for data_type in arguments.data_type:
-            base_path = os.path.join(arguments.ftp_path, f"species/{species_name}/{accession_name}/{data_type}")
-            print(f"base_path: {base_path}")
-            species_info[data_type] = [ i for i in [ os.path.join(base_path, genebuild_update) , 
-                                                                           os.path.join(base_path, genebuild_inital)] if os.path.exists(i) ]
+        logger.info(f"Processing Species {species_info['name']}")
+        logger.info(f"Fetch annotation source from DB  {species_info['dbname']}")
+        processed_any_species = True
+        try: 
+            core_metadata = get_annotations_source(species_info['dbname'], arguments.coredb_url)
+            #if 'species.annotation_source' not in core_metadata.keys() or core_metadata['species.annotation_source'] == '' :
+            if core_metadata.get('species.annotation_source', '')  ==  '':
+                core_metadata['species.annotation_source'] = 'ensembl' 
+                    
+
+            species_name = "_".join(species_info['display_name'].split(' ')[0:2]) #Homo sapiens (Human) - GCA_018503265.1 
+            accession_name = species_info['assembly_accession'] #GCA_009914755.4
+            genebuild_inital = core_metadata.get('genebuild.initial_release_date').replace('-','_') if core_metadata.get('genebuild.initial_release_date', None) else ''
+            genebuild_update = core_metadata.get('genebuild.last_geneset_update').replace('-','_') if core_metadata.get('genebuild.initial_release_date', None) else ''
+            
+            for data_type in arguments.data_type:
+                base_path = os.path.join(arguments.ftp_path, f"species/{species_name}/{accession_name}/{data_type}")
+                timestamp_base_path = os.path.join(arguments.ftp_path, f"timestamped/species/{species_name}/{accession_name}/{data_type}")
+                target_path = os.path.join(arguments.ftp_path, f"species/{species_name}/{accession_name}/{core_metadata['species.annotation_source']}") 
+                timestamp_target_path = os.path.join(arguments.ftp_path, f"timestamped/species/{species_name}/{accession_name}/{core_metadata['species.annotation_source']}")
+                
+                subdir_paths = []
+                if os.path.exists(base_path) :
+                    subdir_paths.append( (base_path,  target_path) ),
+                    move_subdir_paths(base_path,  target_path )
+                                                    
+                            
+                if os.path.exists(timestamp_base_path):
+                    
+                    subdir_paths.append( (timestamp_base_path, timestamp_target_path))
+                    move_subdir_paths(timestamp_base_path,  timestamp_target_path)
+
+                    
+                set_broken_symlink(target_path, data_type, core_metadata['species.annotation_source'], script_path)
+                species_info[data_type] = subdir_paths
+                #species_info[data_type] = [ i for i in [ os.path.join(base_path, genebuild_update) , 
+                #                                                               os.path.join(base_path, genebuild_inital)] if os.path.exists(i) ]
+
+            
+            logger.info(f"Sub directory changed for {species_info['name']} with details  {species_info}")
+        except Exception as e:
+            logger.error(f"Failed to process species {species_info['name']}, error: {str(e)} ")
+    
+    if not processed_any_species:
+        logger.error(f"No species found for provided {arguments} , check ens_version & rr_version ")
         
-        print(species_info)
+            
+        
+            
   
